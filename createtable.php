741573a5b8a6ed89b05219a622feee59838b51ba
MFANAMKHULU <katlehomofokeng179@gmail.com>
2023-07-17 10:04:29 +0200

    myfirstpush

---
+++ createtable.php
@@ -0,0 +1,33 @@
+<?php
+/*$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "katdb"; 
+
+
+# Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+# Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+
+# create table
+$sql = "CREATE TABLE TechCodeInterns (
+ learnerid INT(6) PRIMARY KEY,   
+internName VARCHAR(60) NOT NULL,
+Surname VARCHAR(60) NOT NULL,
+# homeAddress VARCHAR(70) NOT NULL,
+empNum INT(10) NOT NULL
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table TechCode interns created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();*/
+?>
+
+<?php
---
+++ limitdata.php
@@ -0,0 +1,24 @@
+<?php
+/*$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "katdb";
+
+# Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+# Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM TechCodeInternLearners LIMIT 3";
+
+if ($conn->query($sql) === TRUE) {
+  echo " Data could be limited ";
+} else {
+  echo "Error Data could not be limited: " . $conn->error;
+}
+
+
+$conn->close();*/
+?>
