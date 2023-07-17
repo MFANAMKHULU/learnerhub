d928257b6e7ba8428797ca861bfdb520988f5b43
MFANAMKHULU <katlehomofokeng179@gmail.com>
2023-07-17 15:39:44 +0200

    myfirstpush

--- getuserinput.php
+++ getuserinput.php
@@ -1,21 +1,21 @@
 
 <html>
 <body>
-    <?php
+    
 #<!-- Declaring variables -->
 
-$name <input type="text" name="name"><br>
-$lastname <input type="text" name="name"><br>
-$id <input type="text" name="name"><br>
-$studentnum <input type="text" name="name"><br>
-$cellno <input type="text" name="name"><br>
-$Email: <input type="text" name="email"><br>
+name <input type="text" name="name"><br>
+lastname <input type="text" name="name"><br>
+id <input type="text" name="name"><br>
+studentnum <input type="text" name="name"><br>
+cellno <input type="text" name="name"><br>
+Email: <input type="text" name="email"><br>
 
 <!-- radio group -->
 <input type="radio" name="male" value="0">Female
-<input type="radio" name="female" value="1">Male
+<input type="radio" name="female" value="1">Male <br>
 
-function determineGenderFromID($id) {
+ <!--function determineGenderFromID($id) {
     // Extract the relevant digits from the ID number
     $genderDigits = substr($idNumber, -2, 1);
 
@@ -25,14 +25,14 @@ function determineGenderFromID($id) {
     } else {
         return 'Male';
     }
-}
+}-->
 #<!--$name = $lastname = $id = $studentnum = $cellno = $Email = "";-->
 
 
 
-<!-- <input type="Confirm "> -->
+ <input type= Submit > 
 
 </form>
->?
+
 </body>
 </html>
