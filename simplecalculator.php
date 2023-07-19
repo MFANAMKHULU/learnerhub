3ce20d09ae77e3af6b1ea9aa63ad6079da1aab25
MFANAMKHULU <katlehomofokeng179@gmail.com>
2023-07-19 09:35:04 +0200

    19july09:00indexpage

--- simplecalculator.php
+++ simplecalculator.php
@@ -30,7 +30,35 @@
         $num1 = $_POST["num1"];
         $num2 = $_POST["num2"];
         $operation = $_POST["operation"];
+
+          # 13:00
+     switch ($operation)
+     {
+         case "add":
+             $result = $num1 + $num2;
+             break;
+             case "subtract":
+                 $result = $num1 - $num2;
+                 break;
+                 case "multiply":
+                     $result = $num1 * $num2;
+                     break;
+                      # 14:00
+                  /* case "divide":
+                    if ($num2 != 0) {
+                        $result = $num1 / $num2;
+                    } else {
+                        $result = "Cannot divide by zero!";
+                    }
+                    break;
+                default:
+                    $result = "Invalid operation";
+                    break;  */
      } 
+     #15:00
+    #
+    echo "Result: " . $result; 
+   }
      ?>
 
 </body>
