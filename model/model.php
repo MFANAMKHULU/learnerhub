ec9de18f2c54bce17c2c135c3b9cfae0ec294eb6
MFANAMKHULU <katlehomofokeng179@gmail.com>
2023-07-26 12:38:51 +0200

    26 july 12:00 updated project

--- model/model.php
+++ model/model.php
@@ -1,60 +1,46 @@
 <?php
 
 final class MyClass{
- // variables   
-private $name;
-private $email;
-private $ID;
-private $CellNumber;
+    // Where we get input and process it
+    // we control how data is represented here
+    public function processinfo($data)
+    {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        #$ID = $_POST['ID'];
+        #$ID = $_POST['CellNumber']
 
-// populate into array
-public function setParam(stdClass $MyClass){
-
-    if(array_key_exists('name', $MyClass)){
-        $this->name = $MyClass->name;
+        $result = $this->processinfo($name,$email);
     }
 
-    if(array_key_exists('email', $MyClass)){
-        $this->email = $MyClass->email;
-    }
-
-    if(array_key_exists('ID', $MyClass)){
-        $this->ID = $MyClass->ID;
-    }
-
-    if(array_key_exist('CellNumber', $MyClass)){
-        $this->CellNumber = $MyClass->CellNumber;
-    }
-  
-}};
-
-
-final class Helper{ 	
-  // Don't touch anything here
-    public static function getUrlParam($name) {
-        if (!array_key_exists($name, $_GET)) {
-            throw new NotFoundException('URL parameter "' . $name . '" not found.');
-        }
-        return $_GET[$name];
-    }
-
-    public static function redirect($page, array $params = []) {
-        header('Location: ' . self::createLink($page, $params));
-        die();
-    }
-
-    public static function createLink($page, array $params = []) {
-        unset($params['page']);
-        return (empty($params))?$page :$page.'&'.http_build_query($params);
-    }
-
-    public static function capitalize($string) {
-        return ucfirst(mb_strtolower($string));
-    }
-
-    public static function escape($string) {
-        return htmlspecialchars($string, ENT_QUOTES);
-    }
 };
 
+final class Helper{ 	
+    // Don't touch anything here
+      public static function getUrlParam($name) {
+          if (!array_key_exists($name, $_GET)) {
+              throw new NotFoundException('URL parameter "' . $name . '" not found.');
+          }
+          return $_GET[$name];
+      }
+  
+      public static function redirect($page, array $params = []) {
+          header('Location: ' . self::createLink($page, $params));
+          die();
+      }
+  
+      public static function createLink($page, array $params = []) {
+          unset($params['page']);
+          return (empty($params))?$page :$page.'&'.http_build_query($params);
+      }
+  
+      public static function capitalize($string) {
+          return ucfirst(mb_strtolower($string));
+      }
+  
+      public static function escape($string) {
+          return htmlspecialchars($string, ENT_QUOTES);
+      }
+  };
+  
 ?>
