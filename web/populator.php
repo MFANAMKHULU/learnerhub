7828bc9d671b196d0ae710b07087a3c3eeed593d
MFANAMKHULU <katlehomofokeng179@gmail.com>
2023-07-27 13:03:28 +0200

    required

--- web/populator.php
+++ web/populator.php
@@ -1,6 +1,7 @@
 <?php
-$pageTitle = "HOMEPAGE";
-$name = "Welcome MFANAMKHULU";
-$message = "<p> THIS IS THE INFOMATION THAT'LL BE POSTED HERE.</p>";
+  $pageTitle = "HOMEPAGE";
+  $name = "Welcome MFANAMKHULU";
+  $message = " THIS IS THE INFOMATION THAT'LL BE POSTED HERE";
+
 ?>
 
