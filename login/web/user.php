c82f2486ebd5fbfa33837a5e5aad7cab7a7aba63
MFANAMKHULU <katlehomofokeng179@gmail.com>
2023-08-01 14:30:57 +0200

    user 14:00

--- login/web/user.php
+++ login/web/user.php
@@ -7,13 +7,13 @@
 
 if(! (array_key_exists('view',$_GET) && in_array($action, $view)))
 {
-    throw  new NotFoundException("View not found");  
+    throw  new NotFoundException("View not found");  // checking if view exists and throwing exception
 }
 
 $errors = [];
     if(array_key_exists('signup', $_POST))
     {
-        #calling variables from classes
+        #calling variables from classes setting and getting
         $data = ['name' => isset($_POST['user']['name'])?$_POST['user']['name'] :'',
             'email' => isset($_POST['user']['email'])?$_POST['user']['email'] :'',
             'ID' => isset($_POST['user']['ID'])?$_POST['user']['ID'] :'',
@@ -22,4 +22,22 @@ $errors = [];
 
             $obj = new User();  
             $errors = $obj->signup($data);
-    }  
+    } 
+    
+    else if(array_key_exists('login', $_POST))
+    {
+        $data = ['email' => isset($_POST['user']['email'])?$_POST['user']['email'] :'',
+                'password' => isset($_POST['user']['password'])?$_POST['user']['password'] :''];
+
+        $obj = new User();  
+        $errors = $obj->login($data);
+
+        if(empty($errors)){
+
+            if($obj->getPassword() != $data['password']){
+                $errors[] = new ValidatorError('Login Failed', 'Check Username or Password And try again');
+            }
+            else{ echo 'Welcome User'; }
+        }
+    }
+    ?>
