a41035e7b8a67ad86342d6f8e5828062921fa7e0
MFANAMKHULU <katlehomofokeng179@gmail.com>
2023-08-08 10:23:04 +0200

    10:00 functions and classes

---
+++ JavaWeb/name.java
@@ -0,0 +1,17 @@
+public class name {
+    private int number;
+    private String name;
+
+    public Name(int number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
