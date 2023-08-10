a0f99249085de28f22450cfc86ffa322dc002dea
MFANAMKHULU <katlehomofokeng179@gmail.com>
2023-08-10 09:17:34 +0200

    09:00 framework

---
+++ JavaFramework/SubmitServlet.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SubmitServlet")
+public class SubmitServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String name = request.getParameter("name"); // Get the submitted name
+
+        // You can process the name here (e.g., store it in a database, display it, etc.)
+
+        response.setContentType("text/html");
+        response.getWriter().println("<h2>Hello, " + name + "!</h2>");
+    }
+}
