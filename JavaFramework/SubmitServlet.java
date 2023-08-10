ee045335a453ab71620f1c67d6c44989d10100d7
MFANAMKHULU <katlehomofokeng179@gmail.com>
2023-08-10 09:31:59 +0200

    09:00

--- JavaFramework/SubmitServlet.java
+++ JavaFramework/SubmitServlet.java
@@ -1,4 +1,6 @@
+
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 public class SubmitServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String name = request.getParameter("name"); // Get the submitted name
