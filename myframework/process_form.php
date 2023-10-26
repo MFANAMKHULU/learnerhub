c0d8cb834089c7d6d495dd5da07e19cb935e50fe
MFANAMKHULU <katlehomofokeng179@gmail.com>
2023-10-26 16:12:06 +0200

    16:00 my code

--- myframework/process_form.php
+++ myframework/process_form.php
@@ -13,28 +13,59 @@ if ($conn->connect_error) {
 }
 
 // Get form data
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
+$first_name = $_POST['name'];
+$last_name = $_POST['lastname'];
 $email = $_POST['email'];
-$phone =$_Post['phone']
-//$role = $_POST['role'];
+$phone = $_POST['cellnumber'];
+$password = $_POST['password'];
+$role = $_POST['role'];
 
-
-// checking if all info is entered 
-if (empty($name) ||empty($lastname) || empty($email) || empty($password) || empty($cellnumber) ) {
+// checking if all info is entered
+if (empty($first_name) || empty($last_name) || empty($email) || empty($password) || empty($phone)) {
     die("All fields are required.");
 }
 
-   //checking if email format is correct
+// checking if email format is correct
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     die("Invalid email format.");
 }
+ // validating password
+ 
+ function validatePassword($password) {
+    // Check if password contains at least one capital letter
+    if (!preg_match('/[A-Z]/', $password)) {
+        return "Password must contain at least one capital letter.";
+    }
 
-if ( 'attendee') {
-    $sql = "INSERT INTO attendees (first_name, last_name, email, phone, password ) VALUES ('$first_name', '$last_name', '$email', '$phone' '$password')";
-} elseif ( 'organizer') {
-    $sql =
-    $sql = "INSERT INTO organizer (organizer_name, organizer_surname, email, phone, password ) VALUES ('$first_name', '$last_name', '$email', '$phone' '$password')";
+    // Check if password contains at least one special character
+    if (!preg_match('/[^a-zA-Z\d]/', $password)) {
+        return "Password must contain at least one special character.";
+    }
+
+    // Check if password contains at least one number
+    if (!preg_match('/\d/', $password)) {
+        return "Password must contain at least one number.";
+    }
+
+    return true; // Password is valid
+}
+
+// Usage
+$password = $_POST['password'];
+$passwordValidationResult = validatePassword($password);
+
+if ($passwordValidationResult === true) {
+    // Password is valid, proceed with registration
+} else {
+    // Password is invalid, show error message
+    die($passwordValidationResult);
+}
+
+
+if ($role == 'attendee') {
+    $sql = "INSERT INTO attendee (first_name, last_name, email, phone, password ) VALUES ('$first_name', '$last_name', '$email', '$phone', '$password')";
+} elseif ($role == 'organizer') {
+    $sql = "INSERT INTO organizer (organizer_name, organizer_surname, email, phone, password ) VALUES ('$first_name', '$last_name', '$email', '$phone', '$password')";
 }
 
 if ($conn->query($sql) === TRUE) {
